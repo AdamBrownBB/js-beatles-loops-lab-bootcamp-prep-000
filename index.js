354521f821a91109ea0db6ceb04c0095e3ab4de5
Adam Brown <adam.brown.betty@gmail.com>
2019-07-01 15:25:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-// add solution here
+/*  create variables that mirror the expected results in the test
+Create empty array
+There are 4 beatles, so make the FOR loop end at 5.
+use PUSH method to add to the array
+templ. literal to get the words right, INDEX to match them up. 
+*/
+
 var musician = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 var instrument = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
@@ -6,12 +12,19 @@ function theBeatlesPlay(musician, instrument) {
   var array = [];
   
   for (let i = 0; i < 4; i++) {
-    array.push(`${musician[i]} plays ${instrument[i]}`)
+    array.push(`${musician[i]} plays ${instrument[i]}`);
   }
   
-  return array
+  return array;
 }
 
+
+/* copy/paste the facts
+make new array to store answers
+use a WHILE loop
+use PUSH to add next fact into the array and add the "!!!"
+increment by 1
+*/
 const facts = [
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
@@ -32,6 +45,15 @@ return newFacts;
   
 }
 
+
+/* make empty array to store this
+  make a DO WHILE loop
+  add text
+  increase num by 1 
+  return the new array until it gets to 15
+  
+
+*/
 function iLoveTheBeatles (num) {
   var array = [];
   
